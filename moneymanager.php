ae35e55c4fd3727bd7dd724b331d9c4d454af5ab
Nikhil Baliga <nikhilbaliga@nikhilbaliga-zovi.(none)>
2013-06-09 02:35:08 +0530

    Featuring IOU

--- moneymanager.php
+++ moneymanager.php
@@ -50,9 +50,7 @@
 
 	<div class="span9">
 
-		<div></div>
-
-		<div id="monthly_table" style="border: solid 1px #eee; padding: 15px;">
+		<div id="monthly_table" style="border: solid 1px #eee; padding: 15px;" class="hide">
 
 			<div class="well">
 				<form class="form-horizontal">
@@ -104,41 +102,45 @@
 			</table>
 		</div>
 
+		<div id="monthly_ious" style="border: solid 1px #eee; padding: 15px;">
+			<div class="well">
+				<form class="form-horizontal">
+					<select class='all_months' ng-model="mt_month" ng-change="month_changed()" ng-options="m.index as m.month for m in all_months">
+					</select>
 
-		<div id="monthly_budget" class="row span9 modal hide">
-			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<select class='all_years span2' ng-model="mt_year" ng-options="each_year as each_year for (key, each_year) in all_years">
+						<!-- <option ng-repeat="(key, each_year) in all_years" value="{{each_year}}">{{each_year}}</option> -->
+					</select>
 
-			<h3>Monthly Budget</h3>
-
-			<table style="width: 75%">
-				<tr>
-					<td>
-						<select class='all_months' ng-options="m.index as m.month for m in all_months" ng-model="budget_month">
-						</select>
-					</td>
-
-					<td>
-						<select class='all_years span2' ng-options="key as each_year for (key, each_year) in all_years" ng-model="budget_year">
-						</select>
-					</td>
-				</tr>
-
-				<tr>
-					<td>I want to spend NOT more than </td>
-					<td><input type="text" class="span2"></td>
-				</tr>
-
-				<tr>
-					<td>I want to save at least </td>
-					<td><input type="text" class="span2"></td>
-				</tr>
-			</table>
-
-			<div class="btn_toolbar">
-				<button class="btn">Cancel</button> 
-				<button class="btn btn-primary">Save</button>
+					<button class="btn btn-success pull-right" id="btn_add_iou" ng-click="btn_add_iou()"><i class="icon-plus icon-white"></i> IOU</button>
+				</form>
 			</div>
 
+			<table class="table table-hover">
+				<tr>
+					<th>Sl</th>
+					<th>Date</th>
+					<th>Narration</th>
+					<th>Amount</th>
+					<th></th>
+					<th></th>
+				</tr>
+
+				<tr ng-repeat="(sl, each_row) in monthly_data">
+					<td>{{sl + 1}}</td>
+					<td>{{each_row.full_date}}</td>
+					<td>{{each_row.narration}}</td>
+					<td ng-class="{label_green: each_row.inc_exp == 'inc', label_red: each_row.inc_exp == 'exp'}">{{each_row.amount}}</td>
+					<td><i class="icon-pencil" ng-click="edit_narration(sl)"></i></td>
+					<td><i class="icon-remove" ng-click="delete_narration(sl)"></i></td>
+				</tr>
+
+				<tr ng-show="monthly_data.length == 0">
+					<td colspan="6" style="text-align: center;">
+						No data to display
+					</td>
+				</tr>
+			</table>
 		</div>
 
 	</div>
@@ -153,6 +155,41 @@
 
 </div>
 
+<div id="monthly_budget" class="row span9 modal hide">
+	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+
+	<h3>Monthly Budget</h3>
+
+	<table style="width: 75%">
+		<tr>
+			<td>
+				<select class='all_months' ng-options="m.index as m.month for m in all_months" ng-model="budget_month">
+				</select>
+			</td>
+
+			<td>
+				<select class='all_years span2' ng-options="key as each_year for (key, each_year) in all_years" ng-model="budget_year">
+				</select>
+			</td>
+		</tr>
+
+		<tr>
+			<td>I want to spend NOT more than </td>
+			<td><input type="text" class="span2"></td>
+		</tr>
+
+		<tr>
+			<td>I want to save at least </td>
+			<td><input type="text" class="span2"></td>
+		</tr>
+	</table>
+
+	<div class="btn_toolbar">
+		<button class="btn">Cancel</button> 
+		<button class="btn btn-primary">Save</button>
+	</div>
+
+</div>
 
 
 <div id="add_narration" class="modal span6 offset4 row hide">
@@ -181,7 +218,7 @@
 
 			<hr>
 
-			<div>
+			<div class="form-horizontal">
 				<input placeholder="Amount" type="text" class="span2" ng-model="amount" required>
 
 				<div class="btn-group" data-toggle="buttons-radio">
@@ -257,6 +294,57 @@
 
 </div>
 
+<div id="add_iou" class="modal span6 offset4 row" ng-controller="ADDIOUController">
+	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+
+	<h3>Add IOU</h3>
+
+	<table class="table">
+		<tr>
+			<td>
+				<div class="btn-group" data-toggle="buttons-radio">
+					<button type="button" class="btn btn-success">I Have To Get Money</button>
+					<button type="button" class="btn btn-danger">I Owe Money</button>
+				</div>
+			</td>
+
+			<td>
+				<select class="span1" ng-model="iou_date" required ng-options="d.index as d.date for d in all_days">
+				</select>
+				<strong>{{all_months[mt_month - 1]['month']}}, {{mt_year}}</strong>
+			</td>
+		</tr>
+	</table>
+
+	
+
+	<input class="span7" type="text" placeholder="Narration">
+
+	<div class="form-horizontal">
+
+		<input type="text" placeholder="Person" class="span3" ng-model="person"> 
+		<input type="text" placeholder="Amount" class="span2" ng-model="amount"> 
+		<button class="btn btn-success" ng-click="add_iou_to_cur_iou()"><i class="icon-white icon-plus"></i></button>
+
+		<div>
+			<div class="row" ng-repeat="(key, each_iou) in cur_iou" style="margin-top: 10px;">
+				<div class="span3" ng-class="{struck: true == each_iou.paid}">{{each_iou.person}}</div>
+				<div class="span2" ng-class="{struck: true == each_iou.paid}">{{each_iou.amount}}</div>
+				<button class="btn" ng-show="each_iou.paid == false">Paid</button>
+				<button class="btn" ng-show="each_iou.paid == true"><i class="icon-repeat"></i></button>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="btn_toolbar">
+		<button class="btn">Cancel</button>
+		<button class="btn btn-primary">Add</button>
+	</div>
+
+
+</div>
+
 
 	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/moneymanager.js"></script>
@@ -270,9 +358,43 @@
 		});
 	</script>
 
+	<script type="text/javascript">
+		function ADDIOUController($scope, $http) {
+			$scope.cur_iou = [
+				{
+					amount: 1000,
+					person: 'Nikhil Baliga',
+					paid: false
+				},
+				{
+					amount: 500,
+					person: 'Amod Pandey',
+					paid: true
+				},
+				{
+					amount: 1500,
+					person: 'Niyaz',
+					paid: false
+				}
+			];
+
+			$scope.add_iou_to_cur_iou = function() {
+				var iou = {};
+				iou['amount'] = $scope.amount;
+				iou['person'] = $scope.person;
+				iou['paid'] = false;
+
+				$scope.cur_iou.push(iou);
+
+				$scope.amount = undefined;
+				$scope.person = undefined;
+			}
+		}
+	</script>
+
 	
 	<style type="text/css">
-	#add_narration {
+	#add_narration, #add_iou {
 		border: solid 1px #EEE;
 		padding: 20px;
 	}
@@ -300,6 +422,10 @@
 	.label_red {
 		color: red;
 		font-weight: bold;
+	}
+
+	.struck {
+		text-decoration: line-through;
 	}
 
 	</style>
