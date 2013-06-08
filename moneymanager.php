a032a226d928f8fc8482e41c10ae14ad065a75d3
Nikhil Baliga <nikhilbaliga@nikhilbaliga-zovi.(none)>
2013-06-08 14:56:54 +0530

    Doctype

--- moneymanager.php
+++ moneymanager.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
 <?php
 	require 'authentication_services.php';
 
@@ -9,8 +12,6 @@
 	echo "<script>var username = '" . $username . "';</script>";
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html ng-app>
 <head>
